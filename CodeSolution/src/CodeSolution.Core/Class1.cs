e2649d4867000868ea4e1e5fe6ffd1a51ef5400d
Kostas <konstantinos.gkaris@gmail.com>
2026-08-13 11:45:51 +0200

    Created the basic API implementation for our project

---
+++ CodeSolution/src/CodeSolution.Core/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace CodeSolution.Core;
+
+public class Class1
+{
+
+}
---
+++ CodeSolution/tests/CodeSolution.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeSolution.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
